090f804ecb068423d13407269153681c97c0789f
jeonghyeong-il <junghyungile@gmail.com>
2022-02-19 18:26:19 +0900

    secret

--- gateway/src/main/kotlin/me/hyungil/gateway/controller/dto/ErrorResponse.kt
+++
@@ -1,8 +0,0 @@
-package me.hyungil.gateway.controller.dto
-
-data class ErrorResponse(val message: String) {
-
-    companion object {
-        fun of(message: String) = ErrorResponse(message)
-    }
-}
